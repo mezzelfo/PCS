fdaf1e389d9324db84c14175dcdea7baf5db2e8c
mezzelfo <luca.mezzelfo@gmail.com>
2019-01-18 11:39:05 +0100

    Ristrutturazione Totale

--- LAiB02 - C/main.c
+++ LAiB02 - C/main.c
@@ -2,195 +2,127 @@
 #include <stdio.h>
 #include <math.h>
 
+#define TOL 1e-10
 
-int allocaMatrice(double*** m,int ndim)
+typedef double Elemento;
+typedef Elemento* Vettore;
+typedef Vettore* Matrice;
+
+Matrice allocaMatrice(const size_t ndim);
+void liberaMatrice(Matrice m, const size_t ndim);
+Vettore allocaVettore(const size_t ndim);
+void fattorizzazioneLU(Matrice* m, Vettore* pivot, const size_t ndim);
+Vettore forwardSubstitution(const Matrice m, const Vettore pivot, const Vettore terminiNoti);
+Vettore backwardSubstitution(const Matrice m, const Vettore pivot, const Vettore terminiNoti);
+
+int main()
 {
-	int i;
-	*m = ((double**)(malloc(ndim * sizeof(double*))));
+	// Variabili
+	size_t ndim;
+	size_t i,j;
+	Matrice m;
+	Vettore pivot;
+	Vettore terminiNoti;
+	Vettore soluzione;
 
-	if (*m == NULL)
-	{
-		fprintf(stderr, "Allocazione non riuscita\n");
-		return -1;
-	}
+	// Istruzioni
+
+	// Acquisisco da standard input la dimensione della matrice
+	printf("Inserisci la dimensione della matrice\n");
+	scanf(" %lu", &ndim);
+
+	// Alloco la matrice
+	m = allocaMatrice(ndim);
+
+	// Alloco vettori per risoluzione sistema lineare
+	terminiNoti = allocaVettore(ndim);
+	soluzione = allocaVettore(ndim);
+	pivot = allocaVettore(ndim);
+
+	// Acquisisco da standard input il vettore dei termini noti
+	printf("Inserisci il vettore dei termini noti\n");
+	for(i=0;i<ndim;i++) scanf(" %lf", &(terminiNoti[i]));
 	
-
-	for (i = 0; i < ndim; ++i)
-	{
-		(*m)[i] = (double*)malloc(ndim * sizeof(double));
-		if ((*m)[i] == NULL)
-		{
-			fprintf(stderr, "Allocazione non riuscita\n");
-			return -1;
-		}
-	}
-	return 0;
-}
-
-double hilbert(int x, int y) {return 1.0/(x+y+1);}
-
-void riempi(double*** m, int ndim, double f(int, int))
-{
-	int i, j;
-	for (i = 0; i < ndim; ++i)
-	{
-		for (j = 0; j < ndim; ++j)
-		{
-			(*m)[i][j] = f(i,j);
-		}
-	}
-}
-
-void stampa(double*** const m, int ndim)
-{
-	int i,j;
-	for (i = 0; i < ndim; ++i)
-	{
-		for (j = 0; j < ndim; ++j)
-		{
-			printf("%f\t", (*m)[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void LUFactor(double*** m,int** pivot, int ndim)
-{
-	int i,j,k;
-	*pivot = (int*)malloc(ndim*sizeof(int));
-	for(i=0; i<ndim; i++) (*pivot)[i]=i;
-
-
-	for (k = 0; k<ndim-1; k++)
-	{
-		int ind_max = k;
-		for (i = k+1; i < ndim; i++)
-		{
-			if(fabs((*m)[i][k]) > fabs((*m)[ind_max][k]))
-			{
-				ind_max = i;
-			}
-		}
-		printf("scambio riga %d con %d\n", ind_max, k);
-		//voglio scambiare la riga k con la riga ind_max
-		double* tmp = (*m)[ind_max];
-		(*m)[ind_max] = (*m)[k];
-		(*m)[k] = tmp;
-
-		int temp = (*pivot)[ind_max];
-		(*pivot)[ind_max] = (*pivot)[k];
-		(*pivot)[k] = temp;
-
-
-		for (i = k+1; i < ndim; i++)
-		{
-			if((*m)[k][k]!=0.0)//Reimplement relative error 1e-15
-			{
-				(*m)[i][k] = (*m)[i][k]/(*m)[k][k];
-				for (j = k+1; j < ndim; j++)
-				{
-					(*m)[i][j] -= (*m)[i][k]*(*m)[k][j];
-				}
-			}
-			else
-			{
-				printf("NOPE\n");
-			}
-		}
-	}
-}
-
-void forwardSolution(double** a, double** x, double* y, int ndim)
-{
-	printf("Forward subs\n");
-	for (int m = 0; m < ndim; m++)
-	{
-		double sum = 0.0;
-		for (int i = 0; i < m; ++i)
-		{
-			sum += a[m][i]*(*x)[i];
-		}
-		(*x)[m] = (y[m] - sum);
-	}
-}
-
-void backwardSolution(double** a, double** x, double* y, int ndim)
-{
-	printf("Backward subs\n");
-	for (int m = ndim-1; m >= 0; m--)
-	{
-		double sum = 0.0;
-		for (int i = m+1; i < ndim; ++i)
-		{
-			sum += a[m][i]*(*x)[i];
-		}
-		(*x)[m] = (y[m] - sum)/a[m][m];
-	}
-}
-
-int	main()
-{
-	int ndim; 			//Dimensione Matrice
-	double** matrix;	//Puntatore a Puntatore a double
-	int* pivot;
-	double *y, *x, *b;
-	int i;
-
-	printf("Inserisci la dimensione della matrice: ");
-	scanf("%d", &ndim);
-
-	printf("Alloco la matrice\n");
-	
-	if(allocaMatrice(&matrix, ndim))
-	{
-		printf("Errore nella memoria\n");
-		return -1;
-	}
-	
-	printf("Riempio la matrice\n");
-	riempi(&matrix,ndim,&hilbert);
-	
-	printf("Fattorizzo la matrice\n");
-	LUFactor(&matrix, &pivot, ndim);
-
-	printf("Stampo la matrice\n");
-	stampa(&matrix, ndim);
-
-	printf("Pivot:\n");
-	for (i = 0; i < ndim; ++i) printf("%d\t", pivot[i]+1);
-	printf("\n");
-
-
-
-	b = (double*)calloc(ndim,  sizeof(double));
-	for (i = 0; i < ndim; ++i) b[pivot[i]]=i+2;
+	// Riempio la matrice con i coeff di Hilbert
+	for(i=0;i<ndim;i++) for(j=0;j<ndim;j++) m[i][j] = 1.0/(i+j+1.0);
+		
+	// Fattorizzo la matrice
+	fattorizzazioneLU(&m, &pivot, ndim);	// Fattorizzo la matrice eseguendo il pivoting.
+											// Nel vettore pivot scrivo le informazioni per ricostruire gli scambi
 
 	
-	y = (double*)calloc(ndim,  sizeof(double));
-	x = (double*)calloc(ndim,  sizeof(double));
-	
-	
-	forwardSolution(matrix, &y, b, ndim);
-	backwardSolution(matrix, &x, y, ndim);
-	
-	printf("Termini noti:\n");
-	for (i = 0; i < ndim; ++i) printf("%f\t", b[i]);
-	printf("\n");
-
-	printf("Solution:\n");
-	for (i = 0; i < ndim; ++i) printf("%f\t", x[i]);
-	printf("\n");
-
-	for(i=0; i<ndim; i++)
-	{
-		free(matrix[i]);
-	}
-	free(matrix);
+	// Libero la memoria allocata
+	liberaMatrice(m, ndim);
 	free(pivot);
-	free(x);
-	free(y);
-	free(b);
+	free(terminiNoti);
+	free(soluzione);
 
-
+	//Termino il programma
 	return 0;
+}
+
+Matrice allocaMatrice(const size_t ndim)
+{
+	size_t i;
+	Matrice m;
+
+	m = (Matrice)malloc(ndim * sizeof(Vettore));
+	
+	if (m == NULL) {
+		fprintf(stderr, "Allocazione non riuscita\n");
+	}
+	
+	for (i = 0; i < ndim; i++) m[i] = allocaVettore(ndim);
+
+	return m;
+}
+
+void liberaMatrice(Matrice m, const size_t ndim)
+{
+	size_t i;
+
+	for (i = 0; i < ndim; i++) free(m[i]);
+	free(m);
+}
+
+Vettore allocaVettore(const size_t ndim)
+{
+	Vettore v;
+
+	v = (Vettore)malloc(ndim * sizeof(Elemento));
+	
+	if (v == NULL) {
+		fprintf(stderr, "Allocazione non riuscita\n");
+	}
+
+	return v;
+}
+
+void fattorizzazioneLU(Matrice* m, Vettore* pivot, const size_t ndim)
+{
+	/*size_t k, i, j;
+
+	for(k=0; k<ndim-1; k++)
+	{
+		// ricerca elemento pivot e scambio delle righe
+		for(i=k+1; i<ndim; i++)
+			if(abs(*(m[k][k])-0.0)>TOL)
+			{
+				*(m[i][k]) = (*m[i][k]) / (*m[k][k]);
+				for(j=k+1; j<ndim; j++)
+					*(m[i][j]) -= (*m[i][k]) * (*m[k][j]);
+			}
+		//else // messaggio di errore
+	}
+	*/
+}
+
+Vettore forwardSubstitution(const Matrice m, const Vettore pivot, const Vettore terminiNoti)
+{
+	return terminiNoti;
+}
+
+Vettore backwardSubstitution(const Matrice m, const Vettore pivot, const Vettore terminiNoti)
+{
+	return terminiNoti;
 }
