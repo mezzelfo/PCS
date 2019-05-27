d64fca2cff3550cb03435b6835d60ccd3db84cf0
mezzelfo <luca.mezzelfo@gmail.com>
2019-05-27 20:06:01 +0200

    Solo LiveScript MATLAB

--- Matlab/Altri/Vettori Matrici e Sistemi Lineari/es4.m
+++
@@ -1,9 +0,0 @@
-function [A] = es4(N,alfa)
-%ES4 Summary of this function goes here
-%   Detailed explanation goes here
-A = diag(alfa*ones(1,N))-diag(ones(1,N-1),-1)-diag(ones(1,N-1),+1);
-A(1,:) = -1;
-A(:,1) = -1;
-A(1,1) = N;
-end
-
--- Matlab/Es 2.6_2.7/naive_sinh.m
+++
@@ -1,6 +0,0 @@
-function [output] = naive_sinh(x)
-%naive_sinh Compute naively sinh
-%   sinh(x) = (exp(x)-exp(-x))/2
-output = (exp(x)-exp(-x))./2;
-end
-
